5d0c18fb0fb931c8e78b7953ce284473edeffec0
williamlibero <williamliberos@gmail.com>
2020-10-26 20:54:49 -0300

    Correção erro de array

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -75,7 +75,7 @@ class Atendimento{
             if(erro){
                 res.status(400).json(erro)
             }else{
-                res.status(200).json(...valores, id)
+                res.status(200).json(resultados)
             }
         })
     }
